415531b9f16a33f52d93a12ebf2628fe3cb2237e
Gabriel Iltis <iltisg@gmail.com>
2015-07-20 14:07:07 -0400

    DATA: Finished debugging download_data.py for the segmentation demo

--- demos/tomography/end-to-end/download_data.py
+++ demos/tomography/end-to-end/download_data.py
@@ -1,53 +1,57 @@
 """
-Run this file to download and install the data files for the geospatial
-tutorial.
+Run this file to download and unzip the data files for the
+segmentation demonstration.
 
-If you have already downloaded the file tutorial_data.zip, no need to
-run this script. Just unzip that file and place the data folder in this
+If you have already downloaded the file shale_demo_data.zip, no need to
+run this script. Just unzip that file and place the data in this
 directory.
 
-Kelsey Jordahl
-SciPy 2015
+In total there are four files included in the .zip. These files include two
+tomography data sets, collected above and below the K-shell absorption edge
+for iron (Fe), and a .yaml file for each data set. The .yaml files contain
+metadata pertinent to the corresponding tomography data set.
+
+The data sets were collected by Gabriel C. Iltis at the National Synchrotron
+Light Source (NSLS), Brookhaven National Laboratory (BNL), in March of 2014.
 
 """
 
 from __future__ import print_function
 
-import os
 import sys
-from shutil import rmtree
+import os
 import zipfile
+import requests
+from clint.textui import progress
+import tempfile
 
-# python 2/3 compatible imports
-try:
-    from StringIO import StringIO as BytesIO
-except ImportError:
-    from io import BytesIO
-try:
-    from urllib2 import urlopen
-except ImportError:
-    from urllib.request import urlopen
+def download_zip(url, path):
+    r = requests.get(url, stream=True)
+    temp = tempfile.NamedTemporaryFile(suffix='.zip')
+    print('Downloading url --> %s\nto --> %s' % (url, temp.name))
+    with open(temp.name, 'wb') as f:
+        total_length = int(r.headers.get('content-length'))
+        for chunk in progress.bar(r.iter_content(chunk_size=1024),
+                                  expected_size=(total_length/1024) + 1):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+    z = zipfile.ZipFile(temp)
+    print("extracting to --> %s" % path)
+    z.extractall(path=path)
 
+# download to this folder
+current_folder = os.sep.join(__file__.split(os.sep)[:-1])
+url = 'https://www.dropbox.com/s/xpxuy9t2e9f4c9i/shale_demo_data.zip?dl=1'
 
-url = 'http://public.enthought.com/~kjordahl/scipy2015/tutorial_data.zip'
-
-
-if sys.version_info[0] < 3:
-    input = raw_input
-
-here = os.path.dirname(os.path.abspath(__file__))
-data_dir = os.path.join(here, 'data')
-
-if os.path.exists(data_dir):
-    conf = input('data directory exists. Delete? [y/n]')
+if os.path.isfile('NSLS_shale_smpl-2_AbvFe.tiff') and os.path.isfile(
+        'NSLS_shale_smpl-2_BlwFe.tiff'):
+    conf = input('data file already exists. Delete? [y/n]')
     if conf.upper() == 'Y':
-        print('deleting existing directory.')
-        rmtree(data_dir)
+        print('deleting existing file.')
+        os.remove('NSLS_shale_smpl-2_AbvFe.tiff')
+        os.remove('NSLS_shale_smpl-2_BlwFe.tiff')
     else:
         sys.exit(1)
 
-print('reading remote file {}'.format(url))
-response = urlopen(url)
-zf = zipfile.ZipFile(BytesIO(response.read()), 'r')
-zf.extractall(here)
-print('Done!')
+download_zip(url, current_folder)
